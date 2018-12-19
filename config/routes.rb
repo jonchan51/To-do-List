c872ca828f409aa8a9935a3f2fc64a61361eb381
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2018-12-19 15:20:10 +0800

    Added SSL and Puma webserver in production. Added Signup for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'welcome/index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :tasks
+  resources :users
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
